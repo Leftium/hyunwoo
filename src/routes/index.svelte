5a3a9d670a31fe451a103666ec523aa54341347c
John-Kim <Leftium@users.noreply.github.com>
2022-08-16 05:29:53 +0900

    Try 100% instead of 100vw, 100vh.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -174,10 +174,10 @@
     }
 
     iframe {
-        max-width: 100vw;
-        max-height: 100vh;
-        width: 100vw;
-        height: 100vh;
+        max-width: 100%;
+        max-height: 100%;
+        width: 100%;
+        height: 100%;
     }
 
     :global(button.more-info) {
