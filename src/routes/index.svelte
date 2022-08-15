1b33dc6b552cdc62c9cf54b531fc64e8dff751e8
John-Kim <Leftium@users.noreply.github.com>
2022-08-16 01:00:20 +0900

    Hello, Naver Maps.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,29 @@
+<script context=module lang=ts>
+    declare const naver: any;
+</script>
+
+<script lang=ts>
+    import { onMount } from 'svelte';
+
+    import {PUBLIC_NAVER_MAPS_CLIENT_ID} from '$env/static/public';
+
+
+    onMount(() => {
+        const mapOptions = {
+            center: new naver.maps.LatLng(37.3595704, 127.105399),
+            zoom: 10
+        };
+
+        const map = new naver.maps.Map('map', mapOptions);
+    });
+
+</script>
+
+<svelte:head>
+    <script src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId={PUBLIC_NAVER_MAPS_CLIENT_ID}"></script>
+</svelte:head>
+
+
 <div id=map />
 
 <style>
